873d66ce5965d3d88e4b6c035c0597cd0c610ae1
AlexePaul <alexepaul2011@yahoo.com>
2022-10-31 18:06:11 +0200

    finished Extra-Homework

--- TemeLab1/TemeSupl1/lesbulan/lesbulan.cpp
+++ TemeLab1/TemeSupl1/lesbulan/lesbulan.cpp
@@ -7,32 +7,52 @@ ofstream fout("lesbulan.out");
 
 vector<vector<int> >edges;
 vector<int> visited;
-vector<int> length;
 
 int n,m,t;
 bool ok;
+
 void reset(){
 	ok = true;
 	edges.clear();
 	edges.resize(n+1);
 	visited.clear();
 	visited.resize(n+1);
-	length.clear();
-	length.resize(n+1);
+}
+
+void removeLeaves(){
+	map <int, int> leaves;
+	vector<int> aux;
+	for(int i = 0; i < n; ++i){
+		if(edges[i].size() == 1){
+			leaves[i] = 1;
+		}
+	}
+	for(int i = 0; i < n; ++i){
+		if(leaves[i] == 0){
+			for(int j = 0; j < edges[i].size(); ++j)
+				if(leaves[edges[i][j]] == 0){
+					aux.push_back(edges[i][j]);
+				}
+			edges[i] = aux;
+			aux.clear();
+		}
+		else{
+			edges[i].clear();
+		}
+	}
 }
 
 void DFS(int node, int father = -1){
 	visited[node] = true;
 	for(int i = 0; i < edges[node].size(); ++i){
-		if(!visited[edges[node][i]]){
-			length[edges[node][i]] = max(length[edges[node][i]], length[node] + 1);
+		if(!visited[edges[node][i]])
 			DFS(edges[node][i], node);
-		}
-		else if(edges[node][i] != father && visited[edges[node][i]] == 1){
+		else if(edges[node][i] != father){
 			ok = false;
 		}
 	}
 }
+
 int main(){
 	fin >> t;
 	for(int i = 0; i < t; ++i){
@@ -41,24 +61,29 @@ int main(){
 		for(int j = 0; j < m; ++j){
 			int x, y;
 			fin >> x >> y;
-			edges[x].push_back(y);
-			edges[y].push_back(x);
+			edges[x-1].push_back(y-1);
+			edges[y-1].push_back(x-1);
 		}
 
-		for(int j = 1; j <= n; ++j){
+		for(int j = 0; j < n; ++j)
 			if(!visited[j])
 				DFS(j);
-			if(ok == false)
+
+		removeLeaves();
+		removeLeaves();
+
+		bool k = true;
+		
+		for(int i = 0; i < n; ++i){
+			if(edges[i].size() > 2){
+				k = false;
 				break;
+			}
 		}
-		int lengthMax = INT_MIN;
-		for(int i = 1; i <= n; ++i){
-			if(length[i] > lengthMax)
-				lengthMax = length[i];
-		}
-		if(ok == false || lengthMax > 2)
-			fout << 0 << '\n';
-		if(ok == true && lengthMax < 3)
+
+		if(k == true && ok == true)
 			fout << 1 << '\n';
+		else
+			fout << 0 << '\n';
 	}
 }
